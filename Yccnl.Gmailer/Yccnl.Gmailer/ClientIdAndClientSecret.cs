c2d9cebc4fe3671cf2475a28a88e23ee95a89030
Albert Starreveld <astarreveld@vxcompany.com>
2022-11-05 18:57:30 +0100

    feat(reauthorizing): Allow the user to reauthorize

--- Yccnl.Gmailer/Yccnl.Gmailer/ClientIdAndClientSecret.cs
+++ Yccnl.Gmailer/Yccnl.Gmailer/ClientIdAndClientSecret.cs
@@ -15,6 +15,7 @@ namespace Yccnl.Gmailer
         private readonly string _clientId;
         private readonly string _clientSecret;
         private readonly string _fromAddress;
+        private readonly bool _forceReauthorize;
 
         /// <summary>
         /// Creates a new instance of the ClientIdAndClientSecret class. Use this class if you are building a desktop app
@@ -24,13 +25,15 @@ namespace Yccnl.Gmailer
         /// <param name="clientId">The client id you've created at https://console.cloud.google.com/apis</param>
         /// <param name="clientSecret">The client secret you've created at https://console.cloud.google.com/apis</param>
         /// <param name="fromAddress">The e-mail address which corresponds with the client_id and client_secret</param>
+        /// <param name="forceReauthorize">The first time you run the application, google will save the credentials to your machine. If you provide other credentials, they will be ignored for a period of time. Unless you force Google to reauthorize.</param>
         /// <exception cref="ArgumentNullException">Throws an ArgumentNullException if either one of the parameters is null</exception>
         /// <exception cref="ArgumentException">You must provide valid values, otherwise the constructor throws an ArgumentException</exception>
-        public ClientIdAndClientSecret(string clientId, string clientSecret, string fromAddress)
+        public ClientIdAndClientSecret(string clientId, string clientSecret, string fromAddress, bool forceReauthorize = false)
         {
             _clientId = clientId;
             _clientSecret = clientSecret;
             _fromAddress = fromAddress ?? throw new ArgumentNullException(nameof(fromAddress));
+            _forceReauthorize = forceReauthorize;
 
             if (string.IsNullOrEmpty(clientId))
             {
@@ -65,6 +68,11 @@ namespace Yccnl.Gmailer
                 new[] { GmailService.Scope.GmailSend },
                 _fromAddress,
                 CancellationToken.None);
+
+            if (_forceReauthorize)
+            {
+                await GoogleWebAuthorizationBroker.ReauthorizeAsync(credential, CancellationToken.None);
+            }
 
             return credential;
         }
